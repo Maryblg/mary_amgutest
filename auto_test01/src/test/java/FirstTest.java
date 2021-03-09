16848f33d2708d53dbd658f1edf268e16b8bfae2
Maryblg <78217393+Maryblg@users.noreply.github.com>
2021-03-09 22:01:52 +0900

    selenide не ищет

--- auto_test01/src/test/java/FirstTest.java
+++ auto_test01/src/test/java/FirstTest.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -5,43 +7,71 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 import static org.testng.Assert.fail;
 
 
 public class FirstTest {
-    private WebDriver driver;
-    /*private String baseUrl;*/
+  private WebDriver driver;/*
+   private String baseUrl;
     private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
+    private StringBuffer verificationErrors = new StringBuffer();*/
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
-        ChromeOptions options = new ChromeOptions();
+       /*ChromeOptions options = new ChromeOptions();
         options.addArguments("--ignore-certificate-errors");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);*/
+      Configuration.browserCapabilities.set
+      Configuration.browser="chrome";
+
     }
 
     @Test
     public void testOne() throws Exception {
-        driver.get("https://app.geteasyqa.com/users/sign_in");
+       open("https://app.geteasyqa.com/users/sign_in");
+      Selenide.$(UIMap.login).click();
+      Selenide.$(UIMap.login).clear();
+      Selenide.$(UIMap.login).sendKeys("maryamguxi@gmail.com");
+
+      Selenide.$(UIMap.password).click();
+      Selenide.$(UIMap.password).clear();
+      Selenide.$(UIMap.password).sendKeys("45test45");
+
+      Selenide.$(UIMap.loginBtn).click();
+
+       /*Selenide.$(By.id("user_email")).click();
+      Selenide.$(By.id("user_email")).clear();
+      Selenide.$(By.id("user_email")).sendKeys("maryamguxi@gmail.com");
+
+        Selenide.$(By.id("user_password")).click();
+        Selenide.$(By.id("user_password")).clear();
+        Selenide.$(By.id("user_password")).sendKeys("45test45");
+
+        Selenide.$(By.name("commit")).click();
+    driver.get("https://app.geteasyqa.com/users/sign_in");
         driver.findElement(By.id("user_email")).click();
         driver.findElement(By.id("user_email")).clear();
         driver.findElement(By.id("user_email")).sendKeys("maryamguxi@gmail.com");
         driver.findElement(By.id("user_password")).click();
         driver.findElement(By.id("user_password")).clear();
         driver.findElement(By.id("user_password")).sendKeys("45test45");
-        driver.findElement(By.name("commit")).click();
+        driver.findElement(By.name("commit")).click();*/
     }
 
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
-        driver.quit();
+        /*driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
+            fail(verificationErrorString);*/
+        close();
     }
-
+/*
     private boolean isElementPresent(By by) {
         try {
             driver.findElement(by);
@@ -73,6 +103,6 @@ public class FirstTest {
         } finally {
             acceptNextAlert = true;
         }
-    }
+    }*/
 }
 
